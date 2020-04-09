de741dda98948886bfb05f84bb7f851810191585
Tanvir Ali <Tanvir.Ali@dentsuaegis.com>
2020-04-09 09:08:46 +0100

    adding font-awesome config in - see sass-superpowers for more info on font awesome setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: {
-    main: "./src/js/index.js",
-    vendor: "./src/js/vendor.js",
-    contact: "./src/js/contact.js",
-  },
-  plugins: [
+	entry: {
+		main: './src/js/index.js',
+		vendor: './src/js/vendor.js',
+		contact: './src/js/contact.js'
+	},
+	plugins: [],
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [ 'html-loader' ]
+			},
+			{
+				test: /\.(svg|png|jpg|gif)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash].[ext]',
+						outputPath: 'imgs'
+					}
+				}
+			},
 
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "imgs"
-          }
-        }
-      },
-
-    ]
-  }
+			{
+				test: /\.(svg|eot|woff|woff2|ttf)$/,
+				use: [ 'file-loader' ]
+			}
+		]
+	}
 };
